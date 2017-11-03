e79e5007aefeef2e3d60c905aef4ca8bb9824c52
Quique <quique@disruptivemedialabs.com>
2017-11-03 13:14:24 +0100

    Create main activity and station list fragment

---
+++ src/com/google/sample/cast/refplayer/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.google.sample.cast.refplayer.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.sample.cast.refplayer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ src/com/google/sample/cast/refplayer/ui/StationListFragment.java
@@ -0,0 +1,21 @@
+package com.google.sample.cast.refplayer.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.sample.cast.refplayer.R;
+
+public class StationListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_station_list, container, false);
+    }
+}
