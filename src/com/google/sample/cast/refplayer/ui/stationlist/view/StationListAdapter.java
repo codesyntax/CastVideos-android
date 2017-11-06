7c4d55ccbbf7f5d0f8cef85304641bb78a8ae25e
Quique <quiquealvarezmonedero@gmail.com>
2017-11-06 10:05:58 +0100

    Create adapter

---
+++ src/com/google/sample/cast/refplayer/ui/stationlist/view/StationListAdapter.java
@@ -0,0 +1,44 @@
+package com.google.sample.cast.refplayer.ui.stationlist.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.sample.cast.refplayer.R;
+import com.google.sample.cast.refplayer.ui.stationlist.model.StationListItemViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StationListAdapter extends RecyclerView.Adapter<StationListItemViewHolder> {
+    private List<StationListItemViewModel> stations;
+
+    public StationListAdapter() {
+        this.stations = new ArrayList<>();
+    }
+
+    public void setStations(List<StationListItemViewModel> stations) {
+        this.stations = stations;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public StationListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.fragment_recycler_list_view, parent, false);
+        return new StationListItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(StationListItemViewHolder holder, int position) {
+        StationListItemViewModel station = stations.get(position);
+        holder.bind(station);
+    }
+
+    @Override
+    public int getItemCount() {
+        return stations.size();
+    }
+}
