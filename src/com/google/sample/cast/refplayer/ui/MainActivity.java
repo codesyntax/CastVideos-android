4d7f591f208a60bb4400bcc1898149af511eec91
Quique <quique@disruptivemedialabs.com>
2017-11-07 11:41:56 +0100

    Add chromecast button

--- src/com/google/sample/cast/refplayer/ui/MainActivity.java
+++ src/com/google/sample/cast/refplayer/ui/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
 import com.google.sample.cast.refplayer.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        CastContext castContext = CastContext.getSharedInstance(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(),
+                                                menu,
+                                                R.id.media_route_menu_item);
+        return true;
     }
 }
