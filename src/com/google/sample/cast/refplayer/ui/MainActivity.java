e20574eeb2476dcd48070af5d09f0b7e4b2e59e4
Quique <quique@disruptivemedialabs.com>
2017-11-07 11:59:42 +0100

    Add chromecast integration

--- src/com/google/sample/cast/refplayer/ui/MainActivity.java
+++ src/com/google/sample/cast/refplayer/ui/MainActivity.java
@@ -8,17 +8,38 @@ import android.view.Menu;
 
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.CastSession;
+import com.google.android.gms.cast.framework.Session;
+import com.google.android.gms.cast.framework.SessionManager;
+import com.google.android.gms.cast.framework.SessionManagerListener;
 import com.google.sample.cast.refplayer.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CastSession castSession;
+    private SessionManager sessionManager;
+    private final SessionManagerListener sessionManagerListener
+            = new MainSessionManagerListener();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        sessionManager = CastContext.getSharedInstance(this).getSessionManager();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        CastContext castContext = CastContext.getSharedInstance(this);
+    }
+
+    @Override
+    protected void onResume() {
+        castSession = sessionManager.getCurrentCastSession();
+        sessionManager.addSessionManagerListener(sessionManagerListener);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sessionManager.removeSessionManagerListener(sessionManagerListener);
+        castSession = null;
     }
 
     @Override
@@ -29,5 +50,52 @@ public class MainActivity extends AppCompatActivity {
                                                 menu,
                                                 R.id.media_route_menu_item);
         return true;
+    }
+
+    private class MainSessionManagerListener implements SessionManagerListener {
+        @Override
+        public void onSessionStarting(Session session) {
+
+        }
+
+        @Override
+        public void onSessionStarted(Session session, String s) {
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onSessionStartFailed(Session session, int i) {
+
+        }
+
+        @Override
+        public void onSessionEnding(Session session) {
+
+        }
+
+        @Override
+        public void onSessionEnded(Session session, int i) {
+            finish();
+        }
+
+        @Override
+        public void onSessionResuming(Session session, String s) {
+
+        }
+
+        @Override
+        public void onSessionResumed(Session session, boolean b) {
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onSessionResumeFailed(Session session, int i) {
+
+        }
+
+        @Override
+        public void onSessionSuspended(Session session, int i) {
+
+        }
     }
 }
