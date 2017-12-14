06f66461e1f045c6d3849fb6a6a0e071d0707339
Quique <quique@disruptivemedialabs.com>
2017-12-14 10:30:00 +0100

    add queue menu item

--- src/com/google/sample/cast/refplayer/ui/MainActivity.java
+++ src/com/google/sample/cast/refplayer/ui/MainActivity.java
@@ -1,9 +1,9 @@
 package com.google.sample.cast.refplayer.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,14 +15,21 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.cast.MediaQueueItem;
+import com.google.android.gms.cast.MediaStatus;
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.CastSession;
 import com.google.android.gms.cast.framework.Session;
 import com.google.android.gms.cast.framework.SessionManager;
 import com.google.android.gms.cast.framework.SessionManagerListener;
+import com.google.android.gms.cast.framework.media.RemoteMediaClient;
 import com.google.sample.cast.refplayer.R;
+import com.google.sample.cast.refplayer.queue.ui.QueueListViewActivity;
 import com.google.sample.cast.refplayer.ui.channellist.ChannelListListener;
 import com.google.sample.cast.refplayer.ui.channellist.view.ChannelListFragment;
+
+import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -37,8 +44,11 @@ public class MainActivity extends AppCompatActivity implements ChannelListListen
     private LinearLayout channelsDrawerItem;
     private AppCompatTextView channelsDrawerItemValue;
     private LinearLayout queueDrawerItem;
+    private AppCompatTextView queueDrawerItemValue;
     private LinearLayout notificationsDrawerItem;
     private LinearLayout aboutDrawerItem;
+    private RemoteMediaClient remoteMediaClient;
+    private RemoteMediaClient.Listener remoteMediaClientListener = new MyRemoteMediaClientListener();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -66,6 +76,15 @@ public class MainActivity extends AppCompatActivity implements ChannelListListen
         channelsDrawerItem = (LinearLayout) findViewById(R.id.drawer_item_channels);
         channelsDrawerItemValue = (AppCompatTextView) findViewById(R.id.drawer_item_channels_value);
         channelsDrawerItem.setOnClickListener(v -> drawerLayout.closeDrawer(drawerMenu));
+        queueDrawerItem = (LinearLayout) findViewById(R.id.drawer_item_queue);
+        queueDrawerItemValue = (AppCompatTextView) findViewById(R.id.drawer_item_queue_value);
+        queueDrawerItemValue.setText(String.valueOf(0));
+        queueDrawerItem.setOnClickListener(v -> goToQueue());
+    }
+
+    private void goToQueue() {
+        drawerLayout.closeDrawer(drawerMenu);
+        startActivity(new Intent(this, QueueListViewActivity.class));
     }
 
     private void setupDrawerLayout() {
@@ -87,15 +106,38 @@ public class MainActivity extends AppCompatActivity implements ChannelListListen
         getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    private RemoteMediaClient getRemoteMediaClient() {
+        CastSession castSession = castContext.getSessionManager().getCurrentCastSession();
+        return (castSession != null && castSession.isConnected())
+                ? castSession.getRemoteMediaClient() : null;
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
+        if (remoteMediaClient == null) {
+            remoteMediaClient = getRemoteMediaClient();
+        }
+        if (remoteMediaClient != null) {
+            remoteMediaClient.addListener(remoteMediaClientListener);
+            MediaStatus mediaStatus = remoteMediaClient.getMediaStatus();
+            List<MediaQueueItem> queueItems =
+                    (mediaStatus == null) ? null : mediaStatus.getQueueItems();
+            if (queueItems == null || queueItems.isEmpty()) {
+                updateDrawerQueue(0);
+            } else {
+                updateDrawerQueue(queueItems.size());
+            }
+        }
         sessionManager.addSessionManagerListener(sessionManagerListener);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        if (remoteMediaClient != null) {
+            remoteMediaClient.removeListener(remoteMediaClientListener);
+        }
         sessionManager.removeSessionManagerListener(sessionManagerListener);
     }
 
@@ -139,6 +181,10 @@ public class MainActivity extends AppCompatActivity implements ChannelListListen
 
         @Override
         public void onSessionStarted(Session session, String s) {
+            remoteMediaClient = getRemoteMediaClient();
+            if (remoteMediaClient != null) {
+                remoteMediaClient.addListener(remoteMediaClientListener);
+            }
             invalidateOptionsMenu();
         }
 
@@ -154,6 +200,10 @@ public class MainActivity extends AppCompatActivity implements ChannelListListen
 
         @Override
         public void onSessionEnded(Session session, int i) {
+            if (remoteMediaClient != null) {
+                remoteMediaClient.removeListener(remoteMediaClientListener);
+            }
+            remoteMediaClient = null;
             invalidateOptionsMenu();
         }
 
@@ -164,6 +214,10 @@ public class MainActivity extends AppCompatActivity implements ChannelListListen
 
         @Override
         public void onSessionResumed(Session session, boolean b) {
+            remoteMediaClient = getRemoteMediaClient();
+            if (remoteMediaClient != null) {
+                remoteMediaClient.addListener(remoteMediaClientListener);
+            }
             invalidateOptionsMenu();
         }
 
@@ -174,7 +228,54 @@ public class MainActivity extends AppCompatActivity implements ChannelListListen
 
         @Override
         public void onSessionSuspended(Session session, int i) {
-
+            if (remoteMediaClient != null) {
+                remoteMediaClient.removeListener(remoteMediaClientListener);
+            }
+            remoteMediaClient = null;
         }
+    }
+
+    private class MyRemoteMediaClientListener implements RemoteMediaClient.Listener {
+
+        @Override
+        public void onStatusUpdated() {
+            updateMediaQueue();
+        }
+
+        @Override
+        public void onQueueStatusUpdated() {
+            updateMediaQueue();
+        }
+
+        @Override
+        public void onMetadataUpdated() {
+        }
+
+        @Override
+        public void onPreloadStatusUpdated() {
+        }
+
+        @Override
+        public void onSendingRemoteMediaRequest() {
+        }
+
+        @Override
+        public void onAdBreakStatusUpdated() {
+        }
+
+        private void updateMediaQueue() {
+            MediaStatus mediaStatus = remoteMediaClient.getMediaStatus();
+            List<MediaQueueItem> queueItems =
+                    (mediaStatus == null) ? null : mediaStatus.getQueueItems();
+            if (queueItems == null || queueItems.isEmpty()) {
+                updateDrawerQueue(0);
+            } else {
+                updateDrawerQueue(queueItems.size());
+            }
+        }
+    }
+
+    private void updateDrawerQueue(int size) {
+        queueDrawerItemValue.setText(String.valueOf(size));
     }
 }
